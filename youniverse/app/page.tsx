d5b442e951d63e6d786c709f07c2e95b267a6ccd
okni-c <oknistay@gmail.com>
2023-12-11 03:43:27 -0600

    Fix eslint

--- youniverse/app/page.tsx
+++ youniverse/app/page.tsx
@@ -1,31 +1,15 @@
 import ProfileList from '@/components/ProfileList'
 import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
 import Header from '@/components/Header'
 
 export default async function Index() {
-  const cookieStore = cookies()
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  const isSupabaseConnected = canInitSupabaseClient()
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <Header />
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-center items-center p-3 text-sm">
-          {isSupabaseConnected && <AuthButton />}
+          <AuthButton />
         </div>
       </nav>
 
