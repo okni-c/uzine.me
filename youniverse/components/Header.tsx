4a6b7dbe5d40ae99fb8a663fce6b9bafe04113fb
okni-c <oknistay@gmail.com>
2023-12-11 19:52:30 -0600

    Removed old code, added logo

--- youniverse/components/Header.tsx
+++ youniverse/components/Header.tsx
@@ -1,20 +1,16 @@
+import Image from "next/image"
+
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 items-center my-12">
-      {/* <div className="flex gap-8 justify-center items-center my-4">
-        <span className="border-l rotate-45 h-6" />
-        <a href="/">
-          Back to homepage
-        </a>
-        <span className="border-l rotate-45 h-6" />
-      </div> */}
-      <h1 className="sr-only">Bento Clone</h1>
-      <p className="text-3xl lg:text-4xl tracking-tighter mx-auto max-w-xl font-bold text-center text-neutral-500">
-      Bento Clone
-      </p>
-
-      <hr className="h-2 my-8 w-full rounded-xl bg-gray-200 border-0 dark:bg-neutral-500" />
-      
+    <div className="flex flex-col gap-2 items-center my-12">
+      <div className="flex flex-col items-center gap-3">
+        <h1 className="sr-only">Widget Alpha</h1>
+        <Image src={'/widget.png'} alt={''} width={20} height={20} className="animate-spin antialiased duration-[2000]" />
+        <p className="text-3xl md:text-4xl tracking-tighter mx-auto max-w-xl font-extrabold text-center text-neutral-600 antialiased">
+          <span className="text-emerald-500 font-black">you</span>niverse <span className="font-mono text-sm align-super">Alpha</span>
+        </p>
+      </div>
+      <hr className="h-[5px] w-full rounded-xl border-0 bg-neutral-500" />
     </div>
   )
 }
