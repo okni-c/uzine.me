25027eab8b9e961b10c4333092d15a7e1bcee9e8
Dallas Yatsinko <oknistay@gmail.com>
2023-12-16 09:30:37 -0600

    Added caption editing to image widget

--- youniverse/components/widgets/ImageWDG.tsx
+++ youniverse/components/widgets/ImageWDG.tsx
@@ -5,6 +5,8 @@ import { v4 as uuidv4 } from 'uuid';
 export default function ImageWDG({ src, userId, caption, supabase, isAdmin, saveFullGrid, setWidgetArray, widgetArray, id }: any) {
 
     const [newPicture, setNewPicture] = useState<any>(src || '/mountains_placeholder.png')
+    const [captionState, setCaptionState] = useState<string>(caption || '')
+
     const prevNewPictureRef = useRef<any>(newPicture);
     const uploadRef = useRef<HTMLInputElement | null>(null)
 
@@ -79,12 +81,6 @@ export default function ImageWDG({ src, userId, caption, supabase, isAdmin, save
                 // Create a new object to avoid modifying the existing one
                 let updatedWidget = { ...widgetArray[thisWidgetIndex] };
 
-                // Check if the 'component_data' property exists
-                if (!updatedWidget.component_data) {
-                    // If it doesn't exist, create it
-                    updatedWidget.component_data = {};
-                }
-
                 // Check if the 'props' property exists
                 if (!updatedWidget.component_data.props) {
                     // If it doesn't exist, create it
@@ -110,21 +106,68 @@ export default function ImageWDG({ src, userId, caption, supabase, isAdmin, save
         }
     }
 
+    const saveCaption = (newCaption: string) => {
+        // Find the widget with the given id
+        let thisWidgetIndex = widgetArray.findIndex((widget: any) => widget.id === id);
+
+        if (thisWidgetIndex !== -1) {
+            // Create a new object to avoid modifying the existing one
+            let updatedWidget = { ...widgetArray[thisWidgetIndex] };
+
+            // Check if the 'props' property exists
+            if (!updatedWidget.component_data.props) {
+                // If it doesn't exist, create it
+                updatedWidget.component_data.props = {};
+            }
+
+            // Update the 'src' property
+            updatedWidget.component_data.props.caption = newCaption;
+
+            // Create a new array with the updated widget
+            let updatedWidgetArray = [...widgetArray];
+            updatedWidgetArray[thisWidgetIndex] = updatedWidget;
+
+            console.log(updatedWidgetArray);
+
+            // Update state with the new array
+            setWidgetArray(updatedWidgetArray);
+            setTimeout(() => saveFullGrid(), 500);
+
+        } else {
+            console.error("Widget with ID not found");
+        }
+    }
+
+    const handleCaptionChange = (e: any) => {
+        e.preventDefault();
+        const newValue = e.currentTarget.value;
+        setCaptionState(newValue);  
+    }
+
+    const handleBlur = () => {
+        saveCaption(captionState);
+    }
+
     return (
         <>
             {!isAdmin ? (
                 <div className='w-full h-full flex flex-col relative rounded-2xl'>
                     <img src={src ? src : '/mountains_placeholder.png'} className='h-full w-full object-cover rounded-2xl pointer-events-none select-none border border-[rgba(12, 12, 12, 0.19)]' />
-                    <p className='bg-white py-1 px-2 rounded-md absolute bottom-4 left-4 text-sm font-semibold drop-shadow-md border border-neutral-300 group'>Caption text</p>
+                    {caption ? (
+                        <p className='bg-white py-1 px-2 rounded-md absolute bottom-4 left-4 text-sm font-semibold drop-shadow-md border border-neutral-300 group'>{caption}</p>
+                    ) : (
+                        null
+                    )}
+
                 </div>
             ) : (
                 <>
                     <Toaster />
                     <div className='w-full h-full flex flex-col relative rounded-2xl'>
                         <input ref={uploadRef} onChange={handleFileUpload} type="file" accept="image/*" size={1048576} className="hidden m-0 p-0 h-0 w-0" />
-                        <button className='group-hover:opacity-100 opacity-0 w-[30px] h-[30px] self-center rounded-lg absolute bottom-0 right-[-10px] z-[99999] flex items-center justify-center bg-[#FFFFFF] border border-[#4545453b] hover:bg-[#d9d9d9] delete-btn-hover transition-all duration-100 ease-linear' onClick={() => clickInput()}><img src='/upload-button.svg' className='h-[15px] opacity-0 group-hover:opacity-100' /></button>
+                        <button className='group-hover:opacity-100 opacity-0 w-[30px] h-[30px] self-center rounded-lg absolute top-[10px] left-[10px] z-[99999] flex items-center justify-center bg-[#FFFFFF] border border-[#4545453b] hover:bg-[#d9d9d9] delete-btn-hover transition-all duration-100 ease-linear' onClick={() => clickInput()}><img src='/upload-button.svg' className='h-[15px] opacity-0 group-hover:opacity-100' /></button>
                         <img src={newPicture} className='h-full w-full object-cover rounded-2xl pointer-events-none select-none border border-[rgba(12, 12, 12, 0.19)]' />
-                        <p className='bg-white py-1 px-2 rounded-md absolute bottom-4 left-4 text-sm font-semibold drop-shadow-md border border-neutral-300 group text-ellipsis'>Caption text!</p>
+                        <input onChange={handleCaptionChange} onBlur={handleBlur} className={`bg-white py-1 px-2 rounded-md mt-[-45px] mx-[15px] w-auto max-w-[300px] text-ellipsis text-sm font-semibold drop-shadow-md border border-neutral-300 group hover:border-neutral-500 ${captionState === '' ? `opacity-0 group-hover:opacity-100` : null}`} value={captionState} placeholder="Type something!" />
                     </div>
                 </>
             )}
